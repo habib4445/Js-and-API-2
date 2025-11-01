f78dad0bbcaed4aff148428059e2e301cdebf096
habib4445 <0124habibur@gmail.com>
2025-11-01 12:52:38 +0600

    fetch

--- json.js
+++ json.js
@@ -7,3 +7,7 @@ const person={
 const newPerson=JSON.stringify(person);
 console.log(typeof person);
 console.log(typeof newPerson);
+
+// const newPerson2=JSON.parse(newPerson);
+// console.log(newPerson2);
+// console.log(typeof newPerson2);
