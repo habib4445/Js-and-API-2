1e628da74fb481e2e7f7e49ec5b0366edfbb0e80
habib4445 <0124habibur@gmail.com>
2025-11-01 11:48:54 +0600

    json.js

---
+++ arraymap.js
@@ -0,0 +1,17 @@
+// const numbers=[1,2,3,4,5,6,7,];
+// const newArray=numbers.map(number=>number+1);
+// console.log(newArray);
+
+
+// const numbers=[1,2,3,4,5,6,7,];
+// const squareArray=numbers.map(element=>element*element);
+// console.log(squareArray);
+
+const friends=['habib','shakib','masum','rabbi'];
+const newfriends=friends.map((element,index)=>
+{
+    console.log(index),
+console.log(element)
+}
+
+)
---
+++ json.js
@@ -0,0 +1,9 @@
+const person={
+    name:'habib',
+    age:22,
+    friends:["ebru","korim","shakib"],
+    status:true,
+}
+const newPerson=JSON.stringify(person);
+console.log(typeof person);
+console.log(typeof newPerson);
